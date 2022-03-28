ae130feba920a9760ecc7bece253fd4f88bfffd7
gumifu <ffumi.66@gmail.com>
2022-03-28 21:44:34 +0900

    next auth admin/adminindex

--- components/MiniProfile.jsx
+++ components/MiniProfile.jsx
@@ -1,23 +1,24 @@
-import React from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const MiniProfile = () => {
+    const { data: session } = useSession();
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
       <div>
-        {/* <img
+        <img
           src={session?.user?.image}
           alt="dummy profile"
           className=" h-16 w-16 object-cover rounded-full cursor-pointer border p-[2px]"
           layout="fill"
-        /> */}
+        />
       </div>
       <div className="flex-1 mx-4">
-        {/* <h2 className="font-bold text-white">{session?.user?.name}</h2> */}
+        <h2 className="font-bold text-white">{session?.user?.name}</h2>
         <h3 className="text-sm text-gray-400">Welcome to leaflets</h3>
       </div>
-      {/* <button onClick={signOut} className="text-blue-400 text-sm font-semibold">
+      <button onClick={signOut} className="text-blue-400 text-sm font-semibold">
         サインアウト
-      </button> */}
+      </button>
     </div>
   );
 };
